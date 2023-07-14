aa9f7cecc17b6e050eb2fbf8d60c4a72437b94cb
Daniel Nickel <92646300+nickel-dev@users.noreply.github.com>
2023-07-14 06:36:38 -0700

    Add typedef for C

---
+++ typedef.h
@@ -0,0 +1,26 @@
+/* date = July 5th 2023 8:31 am */
+
+#ifndef TYPEDEF_H
+#define TYPEDEF_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef int8_t         i8;
+typedef int16_t        i16;
+typedef int32_t        i32;
+typedef int64_t        i64;
+
+typedef uint8_t        u8;
+typedef uint16_t       u16;
+typedef uint32_t       u32;
+typedef uint64_t       u64;
+
+typedef float          f32;
+typedef double         f64;
+
+#define function         static
+#define local_persist    static
+#define global           static
+
+#endif //TYPEDEF_H
